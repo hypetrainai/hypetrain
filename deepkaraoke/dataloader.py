Fix data normalization in preprocessing. Switch back to L2 loss.

deepkaraoke/dataloader.py
@@ -27,7 +27,7 @@ class KaraokeDataLoader(object):
             for k, (on_vocal, off_vocal) in self.data.items():
                 on_vocal = utils.Convert16BitToFloat(on_vocal)
                 off_vocal = utils.Convert16BitToFloat(off_vocal)
-                norm = np.amax(np.abs(on_vocal + off_vocal))
+                norm = np.amax([np.abs(on_vocal), np.abs(off_vocal)])
                 self.data[k] = (on_vocal / norm, off_vocal / norm)
 
     def get_random_batch(self, sample_length=None, batch_size=None):
@@ -57,7 +57,7 @@ class KaraokeDataLoader(object):
         ]
 
     def get_single_segment(self, extract_idx=0, start_value=3000000, sample_length=200000):
-        
+
         #print(sample_length)
         name = list(self.data.keys())[extract_idx]
         sample_length = sample_length or len(self.data[name][0])

deepkaraoke/fftmodel.py
@@ -53,8 +53,8 @@ class Generator(Network):
         predicted_imag = prediction[:, -fft_channels:]
         gt_real = torch.Tensor(np.real(data['offvocal_stft'])).cuda()
         gt_imag = torch.Tensor(np.imag(data['offvocal_stft'])).cuda()
-        loss = torch.mean(torch.abs(predicted_real - gt_real) +
-                          torch.abs(predicted_imag - gt_imag))
+        loss = torch.mean((predicted_real - gt_real)**2 +
+                          (predicted_imag - gt_imag)**2)
         return loss
 
     def predict(self, data, summary_prefix=''):
@@ -133,8 +133,8 @@ class AutoregressiveGenerator(Generator):
         predicted_imag = prediction[:, -fft_channels:]
         gt_real = torch.Tensor(np.real(data['offvocal_stft'])).cuda()
         gt_imag = torch.Tensor(np.imag(data['offvocal_stft'])).cuda()
-        loss = torch.mean(torch.abs(predicted_real - gt_real) +
-                          torch.abs(predicted_imag - gt_imag))
+        loss = torch.mean((predicted_real - gt_real)**2 +
+                          (predicted_imag - gt_imag)**2)
 
         if self.current_step % 1000 == 0:
           self._summary_writer.add_image(