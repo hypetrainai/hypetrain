07953bd671ed2d2b7b6d7c4505136e579cff28db
Jonathan Shen <jonathanasdf@hotmail.com>
2019-03-16 16:51:20 -0700

    Add UNet-style skip connections between encoder and decoder.

--- deepkaraoke/splitter.py
+++ deepkaraoke/splitter.py
@@ -12,41 +12,48 @@ from GLOBALS import FLAGS, GLOBAL
 _EMB_REDUCTION_FACTOR = 8
 _EMB_DIM = 1024
 _CONV_CHANNELS = 256
+_CONV_LAYERS = 24
+_CONV_DILATION_CYCLE = 8
 
 
 class Encoder(nn.Module):
 
     def __init__(self):
         super(Encoder, self).__init__()
-        layer_defs = []
-        layer_defs.append(submodules.conv_1d(_EMB_REDUCTION_FACTOR, _CONV_CHANNELS))
-        for i in range(24):
-            layer_defs.append(submodules.ResNetModule1d(_CONV_CHANNELS, _CONV_CHANNELS, kernel_size=3, pad=1, dilation=2**(i%8)))
-        layer_defs.append(submodules.conv_1d(_CONV_CHANNELS, 2 * _EMB_DIM))
-        self._model = nn.Sequential(*layer_defs)
+        self._layers = nn.ModuleList()
+        self._layers.append(submodules.conv_1d(_EMB_REDUCTION_FACTOR, _CONV_CHANNELS))
+        for i in range(_CONV_LAYERS):
+            self._layers.append(submodules.ResNetModule1d(_CONV_CHANNELS, _CONV_CHANNELS, kernel_size=3, pad=1, dilation=2**(i % _CONV_DILATION_CYCLE)))
+        self._output = submodules.conv_1d(_CONV_CHANNELS, 2 * _EMB_DIM)
 
     def forward(self, x):
         x = x.view([x.size(0), _EMB_REDUCTION_FACTOR, -1])
-        embs = self._model.forward(x)
-        return torch.split(embs, _EMB_DIM, dim=1)
+        layer_outs = []
+        for layer in self._layers:
+            out = layer.forward(x)
+            layer_outs.append(out)
+            x = out
+        embs = self._output.forward(x)
+        return torch.split(embs, _EMB_DIM, dim=1) + (layer_outs,)
 
 
 class Decoder(nn.Module):
 
     def __init__(self):
         super(Decoder, self).__init__()
-        layer_defs = []
-        layer_defs.append(submodules.conv_1d(_EMB_DIM, _CONV_CHANNELS))
-        for i in range(24):
-            layer_defs.append(submodules.ResNetModule1d(_CONV_CHANNELS, _CONV_CHANNELS, kernel_size=3, pad=1, dilation=2**(i%8)))
-        layer_defs.append(submodules.conv_1d(_CONV_CHANNELS, _EMB_REDUCTION_FACTOR))
-        self._model = nn.Sequential(*layer_defs)
+        self._input = submodules.conv_1d(_EMB_DIM, _CONV_CHANNELS)
+        self._layers = nn.ModuleList()
+        for i in range(_CONV_LAYERS):
+            self._layers.append(submodules.ResNetModule1d(_CONV_CHANNELS, _CONV_CHANNELS, kernel_size=3, pad=1, dilation=2**(i % _CONV_DILATION_CYCLE)))
+        self._layers.append(submodules.conv_1d(_CONV_CHANNELS, _EMB_REDUCTION_FACTOR))
 
-    def forward(self, x):
-        prediction = self._model.forward(x)
-        prediction = prediction.view([prediction.size(0), -1])
-        prediction = torch.clamp(prediction, -1.0, 1.0)
-        return prediction
+    def forward(self, x, encoder_layer_outs):
+        x = self._input.forward(x)
+        for layer, encoder_layer_out in zip(self._layers, reversed(encoder_layer_outs)):
+            x = layer.forward(x + encoder_layer_out)
+        x = x.view([x.size(0), -1])
+        x = torch.clamp(x, -1.0, 1.0)
+        return x
 
 
 class Model(nn.Module):
@@ -59,19 +66,19 @@ class Model(nn.Module):
 
     def forward(self, instr, vocal):
         # emb1 is pushed to have only instrumental information and emb2 vocals.
-        instr_emb1, instr_emb2 = self._encoder.forward(instr)
-        mixed_emb1, mixed_emb2 = self._encoder.forward(instr + vocal)
-        vocal_emb1, vocal_emb2 = self._encoder.forward(vocal)
+        instr_emb1, instr_emb2, _ = self._encoder.forward(instr)
+        mixed_emb1, mixed_emb2, encoder_layer_outs = self._encoder.forward(instr + vocal)
+        vocal_emb1, vocal_emb2, _ = self._encoder.forward(vocal)
 
         emb_loss = torch.mean((instr_emb1 - mixed_emb1) ** 2 + (vocal_emb2 - mixed_emb2) ** 2)
         GLOBAL.summary_writer.add_scalar('loss_train/emb_loss', emb_loss, GLOBAL.current_step)
         emb_norm_loss = torch.mean(instr_emb2 ** 2 + vocal_emb1 ** 2)
         GLOBAL.summary_writer.add_scalar('loss_train/emb_norm_loss', emb_norm_loss, GLOBAL.current_step)
 
-        instr_pred = self._instr_decoder.forward(mixed_emb1)
+        instr_pred = self._instr_decoder.forward(mixed_emb1, encoder_layer_outs)
         instr_loss = torch.mean((instr - instr_pred) ** 2)
         GLOBAL.summary_writer.add_scalar('loss_train/instr_loss', instr_loss, GLOBAL.current_step)
-        vocal_pred = self._vocal_decoder.forward(mixed_emb2)
+        vocal_pred = self._vocal_decoder.forward(mixed_emb2, encoder_layer_outs)
         vocal_loss = torch.mean((vocal - vocal_pred) ** 2)
         GLOBAL.summary_writer.add_scalar('loss_train/vocal_loss', vocal_loss, GLOBAL.current_step)
 
@@ -82,8 +89,8 @@ class Model(nn.Module):
         pad_size = (_EMB_REDUCTION_FACTOR - overflow) % _EMB_REDUCTION_FACTOR
         if pad_size:
           mixed = torch.pad(mixed, [(0, 0), (0, pad_size)])
-        _, mixed_emb2 = self._encoder.forward(mixed)
-        vocal_pred = self._vocal_decoder.forward(mixed_emb2)
+        _, mixed_emb2, encoder_layer_outs = self._encoder.forward(mixed)
+        vocal_pred = self._vocal_decoder.forward(mixed_emb2, encoder_layer_outs)
         return vocal_pred[:, :vocal_pred.shape[1] - pad_size]
 
 
@@ -118,7 +125,7 @@ class Generator(Network):
             chunk_end = min(data[0].shape[1], i + chunk_size)
             end = min(data[0].shape[1], chunk_end + context_size)
             data_i = data[0][:, start:end] + data[1][:, start:end]  # mixed = instr + vocal
-            prediction_i = self.model.predict(data_i)[0].detach().cpu().numpy()
+            prediction_i = self.model.module.predict(data_i)[0].detach().cpu().numpy()
             prediction = np.concatenate((prediction, prediction_i[i - start:chunk_end - start]))
         # since we predicted vocals only, do some math to get off_vocal.
         # prediction = data[0] - prediction
