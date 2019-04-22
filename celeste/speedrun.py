c17501944ca12d84229a0f0391cdaa56ece15e56
Jonathan Shen <jonathanasdf@hotmail.com>
2019-04-22 00:28:02 -0700

    Send BUTTON_A every frame.

--- celeste/speedrun.py
+++ celeste/speedrun.py
@@ -7,8 +7,10 @@ SIZE_UNSIGNED_LONG = 8
 SIZE_TIMESPEC = 16
 SIZE_GAMEINFO_STRUCT = 36
 SIZE_WINDOW_STRUCT = 8
+SIZE_ALL_INPUTS_STRUCT = 200
 
-def startFrameMessages():
+
+def startNextFrame():
   msg = pylibtas.receiveMessage()
   while msg != pylibtas.MSGB_START_FRAMEBOUNDARY:
     if msg == pylibtas.MSGB_WINDOW_ID:
@@ -16,7 +18,7 @@ def startFrameMessages():
     elif msg == pylibtas.MSGB_ALERT_MSG:
       print(pylibtas.receiveString())
     elif msg == pylibtas.MSGB_ENCODE_FAILED:
-      return True
+      return False
     elif msg == pylibtas.MSGB_FRAMECOUNT_TIME:
       pylibtas.ignoreData(SIZE_UNSIGNED_LONG)
       pylibtas.ignoreData(SIZE_TIMESPEC)
@@ -28,13 +30,21 @@ def startFrameMessages():
     elif msg == pylibtas.MSGB_ENCODING_SEGMENT:
       pylibtas.ignoreData(SIZE_INT)
     elif msg == pylibtas.MSGB_QUIT:
-      return True
+      return False
     if msg == -1:
       print("The connection to the game was lost.")
-      return True
+      return False
     msg = pylibtas.receiveMessage()
-  pylibtas.sendMessage(pylibtas.MSGN_START_FRAMEBOUNDARY);
-  return False
+  pylibtas.sendMessage(pylibtas.MSGN_START_FRAMEBOUNDARY)
+  return True
+
+
+def processFrame():
+  ai = pylibtas.AllInputs()
+  press = pylibtas.SingleInput()
+  press.type = pylibtas.SingleInput.BUTTON_A
+  ai.setInput(press, 1)
+  return ai
 
 
 def Speedrun():
@@ -62,12 +72,14 @@ def Speedrun():
 
     pylibtas.sendMessage(pylibtas.MSGN_CONFIG)
     shared_config = pylibtas.SharedConfig()
-    shared_config.initial_framecount = 0
     shared_config.running = True
+    shared_config.nb_controllers = 1
+    shared_config.initial_framecount = 0
     shared_config.incremental_savestates = False
     shared_config.prevent_savefiles = False
-    shared_config.recycle_threads = False
-    shared_config.main_gettimes_threshold = [-1, -1, -1, 100, -1, -1];
+    shared_config.recycle_threads = True
+    shared_config.write_savefiles_on_exit = False
+    shared_config.main_gettimes_threshold = [-1, -1, -1, 100, -1, -1]
     pylibtas.sendSharedConfig(shared_config)
 
     pylibtas.sendMessage(pylibtas.MSGN_ENCODING_SEGMENT)
@@ -75,12 +87,13 @@ def Speedrun():
 
     pylibtas.sendMessage(pylibtas.MSGN_END_INIT)
 
-    while True:
-      quit = startFrameMessages()
-      if quit:
-        break
+    while startNextFrame():
+      ai = processFrame()
+      pylibtas.sendMessage(pylibtas.MSGN_ALL_INPUTS)
+      pylibtas.sendData(ai, SIZE_ALL_INPUTS_STRUCT)
+      pylibtas.sendMessage(pylibtas.MSGN_END_FRAMEBOUNDARY)
 
-    print('Goodbye world!')
+    os.wait()
 
 
 if __name__ == "__main__":
