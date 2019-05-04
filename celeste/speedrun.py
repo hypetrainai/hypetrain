2e450ed88e1cf8aa111245322eb356e2388a2d14
zhaochenML <ZChen89@gmail.com>
2019-05-04 16:06:26 -0700

    added various butotn inputs

--- celeste/speedrun.py
+++ celeste/speedrun.py
@@ -13,8 +13,8 @@ SIZE_WINDOW_STRUCT = 8
 
 
 game_pid = -1
-window_width = 1280
-window_height = 720
+window_width = 960
+window_height = 540
 
 
 def startNextFrame():
@@ -43,13 +43,38 @@ def startNextFrame():
     msg = pylibtas.receiveMessage()
   pylibtas.sendMessage(pylibtas.MSGN_START_FRAMEBOUNDARY)
 
+button_Dict = {
+    'a': pylibtas.SingleInput.IT_CONTROLLER1_BUTTON_A,
+    'b': pylibtas.SingleInput.IT_CONTROLLER1_BUTTON_B,
+    'x': pylibtas.SingleInput.IT_CONTROLLER1_BUTTON_X,
+    'y': pylibtas.SingleInput.IT_CONTROLLER1_BUTTON_Y,
+    'u': pylibtas.SingleInput.IT_CONTROLLER1_BUTTON_DPAD_UP,
+    'd': pylibtas.SingleInput.IT_CONTROLLER1_BUTTON_DPAD_DOWN,
+    'l': pylibtas.SingleInput.IT_CONTROLLER1_BUTTON_DPAD_LEFT,
+    'r': pylibtas.SingleInput.IT_CONTROLLER1_BUTTON_DPAD_RIGHT,
+    'rt': pylibtas.SingleInput.IT_CONTROLLER1_AXIS_TRIGGERRIGHT
+}
 
-def processFrame(prev_inputs, frame):
+def processFrame(prev_inputs, frame, input=None):
   new_inputs = pylibtas.AllInputs()
   new_inputs.emptyInputs()
-  a_button = pylibtas.SingleInput()
-  a_button.type = pylibtas.SingleInput.IT_CONTROLLER1_BUTTON_A
-  new_inputs.setInput(a_button, 1 - prev_inputs.getInput(a_button))
+
+  buttons_pushed = []
+
+  for button in input:
+      if button not in button_Dict:
+        continue
+      new_button = pylibtas.SingleInput()
+      new_button.type = button_Dict[button]
+      buttons_pushed.append(new_button)
+  
+  for button in buttons_pushed:
+      new_inputs.setInput(button, 1)
+    
+  #a_button = pylibtas.SingleInput()
+  #a_button.type = pylibtas.SingleInput.IT_CONTROLLER1_BUTTON_A
+  #new_inputs.setInput(a_button, 1 - prev_inputs.getInput(a_button))
+  #new_inputs.setInput(a_button, 1)
   return new_inputs
 
 
@@ -117,10 +142,78 @@ def Speedrun():
     pylibtas.sendAllInputs(ai)
     pylibtas.sendMessage(pylibtas.MSGN_END_FRAMEBOUNDARY)
 
+    
+def LaunchGame():
+  pylibtas.removeSocket()
+  pylibtas.launchGameThread(
+      'CelesteLinux/Celeste.bin.x86_64',
+      'libTAS/build64/libtas.so',
+      '',  # gameargs
+      0,  # startframe
+      'lib64',
+      os.path.dirname(os.path.abspath(__file__)),
+      pylibtas.SharedConfig.LOGGING_TO_CONSOLE,
+      True,  # opengl_soft
+      '',  # llvm_perf
+      False,  # attach_gdb
+  )
+  pylibtas.initSocketProgram()
+
+  msg = pylibtas.receiveMessage()
+  while msg != pylibtas.MSGB_END_INIT:
+    if msg == pylibtas.MSGB_PID:
+      global game_pid
+      _, game_pid = pylibtas.receiveInt()
+    else:
+      raise RuntimeError('Unexpected message %d in init!' % msg)
+    msg = pylibtas.receiveMessage()
+
+  pylibtas.sendMessage(pylibtas.MSGN_CONFIG)
+  shared_config = pylibtas.SharedConfig()
+  shared_config.nb_controllers = 1
+  shared_config.incremental_savestates = False
+  shared_config.prevent_savefiles = False
+  shared_config.recycle_threads = False
+  shared_config.write_savefiles_on_exit = False
+  shared_config.main_gettimes_threshold = [-1, -1, -1, 100, -1, -1]
+  pylibtas.sendSharedConfig(shared_config)
+
+  pylibtas.sendMessage(pylibtas.MSGN_ENCODING_SEGMENT)
+  pylibtas.sendData(0, SIZE_INT)
+
+  pylibtas.sendMessage(pylibtas.MSGN_END_INIT)
+
+
+
+    
+def AdvanceFrameWithInput():  
+    while True:
+        startNextFrame()
+        msg = pylibtas.receiveMessage()
+        assert msg == pylibtas.MSGB_FRAME_DATA, msg
+        _, actual_window_width = pylibtas.receiveInt()
+        _, actual_window_height = pylibtas.receiveInt()
+        assert actual_window_width == window_width and actual_window_height == window_height
+        _, size = pylibtas.receiveInt()
+        received, frame = pylibtas.receiveArray(size)
+        assert received == size, (size, received)
+
+        frame = np.reshape(frame, [window_height, window_width, 4])[:, :, :3]
+        
+        ai = pylibtas.AllInputs()
+        ai.emptyInputs()
+        button_input = input('Buttons please! (comma separated)').split(',')
+        ai = processFrame(ai, frame, input = button_input)
+        pylibtas.sendMessage(pylibtas.MSGN_ALL_INPUTS)
+        pylibtas.sendAllInputs(ai)
+        pylibtas.sendMessage(pylibtas.MSGN_END_FRAMEBOUNDARY)
+    
 
 if __name__ == "__main__":
   try:
-    Speedrun()
+    #Speedrun()
+    LaunchGame()
+    AdvanceFrameWithInput()
   except:
     if game_pid != -1:
       print('killing game %d' % game_pid)
