186d08f1227e797c2ddd6f3bb3850fcfa67859d7
Jonathan Shen <jonathanasdf@hotmail.com>
2019-08-17 22:07:52 -0700

    Formatting.

--- celeste/model.py
+++ celeste/model.py
@@ -43,8 +43,7 @@ class ConvModel(Model):
     # [time, channels, height, width] -> [time * channels, height, width]
     input_frames = torch.reshape(input_frames, [-1, FLAGS.image_height, FLAGS.image_width])
     input_frames = torch.cat([input_frames, self.extra_channels[i]], 0)
-    input_frames = input_frames.unsqueeze(0)
-    return input_frames
+    return input_frames.unsqueeze(0)
 
 
 class ResNetIm2Value(ConvModel):
@@ -92,143 +91,148 @@ class ResNetIm2Value(ConvModel):
       out = F.softmax(out, 1)
     return out
 
-def agg_node(in_planes, out_planes):
-    return nn.Sequential(
-        submodules.convbn(in_planes, in_planes, kernel_size=3, stride=1, pad=1),
-        submodules.convbn(in_planes, out_planes, kernel_size=3, stride=1, pad=1),
-    )
 
-def smooth(in_planes, out_planes, ks = 3):
-    return nn.Sequential(
-        submodules.convbn(in_planes, out_planes, kernel_size=ks, stride=1, pad=1)
-    )
+def agg_node(in_planes, out_planes):
+  return nn.Sequential(
+    submodules.convbn(in_planes, in_planes, kernel_size=3, stride=1, pad=1),
+    submodules.convbn(in_planes, out_planes, kernel_size=3, stride=1, pad=1),
+  )
+
+
+def smooth(in_planes, out_planes, ks=3):
+  return nn.Sequential(
+    submodules.convbn(in_planes, out_planes, kernel_size=ks, stride=1, pad=1)
+  )
+
 
 def upshuffle(in_planes, out_planes, upscale_factor):
-    return nn.Sequential(
-        nn.Conv2d(in_planes, out_planes*upscale_factor**2, kernel_size=3, stride=1, padding=1),
-        nn.PixelShuffle(upscale_factor),
-        nn.ReLU()
-    )
+  return nn.Sequential(
+    nn.Conv2d(in_planes, out_planes*upscale_factor**2, kernel_size=3, stride=1, padding=1),
+    nn.PixelShuffle(upscale_factor),
+    nn.ReLU()
+  )
+
 
 class FPNNet(ConvModel):
-    def __init__(self, in_dim, out_dim, pretrained=True, fixed_feature_weights=False, use_softmax=True):
-        super(FPNNet, self).__init__()
 
-        resnet = resnet101(pretrained=pretrained)
+  def __init__(self, in_dim, out_dim, pretrained=True, fixed_feature_weights=False, use_softmax=True):
+    super(FPNNet, self).__init__()
 
-        # Freeze those weights
-        if fixed_feature_weights:
-            for p in resnet.parameters():
-                p.requires_grad = False
-        self.use_softmax = use_softmax
-        separate_dims = in_dim - 3
-        self.layer0_sep = nn.Sequential(submodules.convbn(separate_dims, 64, kernel_size = 7, stride = 2, pad=3),
-                                        nn.MaxPool2d(2,2))
+    resnet = resnet101(pretrained=pretrained)
 
-        self.layer0 = nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool)
-        self.layer1 = nn.Sequential(resnet.layer1)
-        self.layer2 = nn.Sequential(resnet.layer2)
-        self.layer3 = nn.Sequential(resnet.layer3)
-        self.layer4 = nn.Sequential(resnet.layer4)
+    # Freeze those weights
+    if fixed_feature_weights:
+      for p in resnet.parameters():
+        p.requires_grad = False
+    self.use_softmax = use_softmax
+    separate_dims = in_dim - 3
+    self.layer0_sep = nn.Sequential(submodules.convbn(separate_dims, 64, kernel_size=7, stride=2, pad=3),
+                                    nn.MaxPool2d(2, 2))
 
-        # Top layer
-        self.toplayer = nn.Conv2d(2048, 256, kernel_size=1, stride=1, padding=1)  # Reduce channels
+    self.layer0 = nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool)
+    self.layer1 = nn.Sequential(resnet.layer1)
+    self.layer2 = nn.Sequential(resnet.layer2)
+    self.layer3 = nn.Sequential(resnet.layer3)
+    self.layer4 = nn.Sequential(resnet.layer4)
 
-        # Lateral layers
-        self.latlayer1 = nn.Conv2d(1024, 256, kernel_size=1, stride=1, padding=1)
-        self.latlayer2 = nn.Conv2d( 512, 256, kernel_size=1, stride=1, padding=1)
-        self.latlayer3 = nn.Conv2d( 256, 256, kernel_size=1, stride=1, padding=1)
+    # Top layer
+    self.toplayer = nn.Conv2d(2048, 256, kernel_size=1, stride=1, padding=1)  # Reduce channels
 
-        # Smooth layers
-        self.smooth1 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
-        self.smooth2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
-        self.smooth3 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+    # Lateral layers
+    self.latlayer1 = nn.Conv2d(1024, 256, kernel_size=1, stride=1, padding=1)
+    self.latlayer2 = nn.Conv2d(512, 256, kernel_size=1, stride=1, padding=1)
+    self.latlayer3 = nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=1)
 
-        # Aggregate layers
-        self.agg1 = agg_node(256, 128)
-        self.agg2 = agg_node(256, 128)
-        self.agg3 = agg_node(256, 128)
-        self.agg4 = agg_node(256, 128)
-        
-        # Upshuffle layers
-        self.up1 = upshuffle(128,128,8)
-        self.up2 = upshuffle(128,128,4)
-        self.up3 = upshuffle(128,128,2)
-        
-        # Predict layers
-        self.predict1 = submodules.convbn(512, 128, kernel_size=3, pad=1, stride=2)
-        self.predict2 = submodules.convbn(128, 128, kernel_size=3, pad=1, stride=1)
-        self.predict3 = submodules.convbn(128, 32, kernel_size=3, pad=1, stride=2)
-        self.predict4 = submodules.convbn(32, 32, kernel_size=3, pad=1, stride=1)
-        self.predict5 = submodules.convbn(32, 8, kernel_size=3, pad=1, stride=1)
-        
-        fc_input = 8*35*61
-        layer_defs_linear = []
-        layer_defs_linear.append(nn.Linear(fc_input, 512))
-        layer_defs_linear.append(nn.ReLU())
-        layer_defs_linear.append(nn.Linear(512, 256))
-        layer_defs_linear.append(nn.ReLU())
-        layer_defs_linear.append(nn.Linear(256, out_dim))
+    # Smooth layers
+    self.smooth1 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+    self.smooth2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+    self.smooth3 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
 
-        self.linops = nn.Sequential(*layer_defs_linear)
-        
-    def _upsample_add(self, x, y):
-        '''Upsample and add two feature maps.
-        Args:
-          x: (Variable) top feature map to be upsampled.
-          y: (Variable) lateral feature map.
-        Returns:
-          (Variable) added feature map.
-        Note in PyTorch, when input size is odd, the upsampled feature map
-        with `F.upsample(..., scale_factor=2, mode='nearest')`
-        maybe not equal to the lateral feature map size.
-        e.g.
-        original input size: [N,_,15,15] ->
-        conv2d feature map size: [N,_,8,8] ->
-        upsampled feature map size: [N,_,16,16]
-        So we choose bilinear upsample which supports arbitrary output sizes.
-        '''
-        _,_,H,W = y.size()
-        return F.upsample(x, size=(H,W), mode='bilinear') + y
+    # Aggregate layers
+    self.agg1 = agg_node(256, 128)
+    self.agg2 = agg_node(256, 128)
+    self.agg3 = agg_node(256, 128)
+    self.agg4 = agg_node(256, 128)
 
-    def forward(self, i):
-        x, sep = self._get_inputs(i)
-        
-        # Bottom-up
-        c1 = self.layer0(x)
-        
-        c1 += self.layer0_sep(sep)
-        
-        c2 = self.layer1(c1)
-        c3 = self.layer2(c2)
-        c4 = self.layer3(c3)
-        c5 = self.layer4(c4)
+    # Upshuffle layers
+    self.up1 = upshuffle(128,128,8)
+    self.up2 = upshuffle(128,128,4)
+    self.up3 = upshuffle(128,128,2)
 
-        # Top-down
-        p5 = self.toplayer(c5)
-        p4 = self._upsample_add(p5, self.latlayer1(c4))
-        p4 = self.smooth1(p4)
-        p3 = self._upsample_add(p4, self.latlayer2(c3))
-        p3 = self.smooth2(p3)
-        p2 = self._upsample_add(p3, self.latlayer3(c2))
-        p2 = self.smooth3(p2)
-        
-        # Top-down predict and refine
-        d5, d4, d3, d2 = self.up1(self.agg1(p5)), self.up2(self.agg2(p4)), self.up3(self.agg3(p3)), self.agg4(p2)
-        _,_,H,W = d2.size()
-        vol = torch.cat( [ F.upsample(d, size=(H,W), mode='bilinear') for d in [d5,d4,d3,d2] ], dim=1 )
-        vol = self.predict5(self.predict4(self.predict3(self.predict2(self.predict1(vol))))).view(vol.shape[0],-1)
-        out = self.linops(vol)
-        
-        if self.use_softmax:
-          out = F.softmax(out, 1)
-        
-        return out
-    
-    def _get_inputs(self, i):
-        input_frames = self.frame_buffer[i:i+FLAGS.context_frames]
-        # [time, channels, height, width] -> [time * channels, height, width]
-        input_frames = torch.reshape(input_frames, [-1, FLAGS.image_height, FLAGS.image_width])
-        current_frame = input_frames[-4:-1]
-        other = torch.cat([input_frames[:-4], input_frames[-1].unsqueeze(0), self.extra_channels[i]])
-        return current_frame.unsqueeze(0), other.unsqueeze(0)
+    # Predict layers
+    self.predict1 = submodules.convbn(512, 128, kernel_size=3, pad=1, stride=2)
+    self.predict2 = submodules.convbn(128, 128, kernel_size=3, pad=1, stride=1)
+    self.predict3 = submodules.convbn(128, 32, kernel_size=3, pad=1, stride=2)
+    self.predict4 = submodules.convbn(32, 32, kernel_size=3, pad=1, stride=1)
+    self.predict5 = submodules.convbn(32, 8, kernel_size=3, pad=1, stride=1)
+
+    fc_input = 8*35*61
+    layer_defs_linear = []
+    layer_defs_linear.append(nn.Linear(fc_input, 512))
+    layer_defs_linear.append(nn.ReLU())
+    layer_defs_linear.append(nn.Linear(512, 256))
+    layer_defs_linear.append(nn.ReLU())
+    layer_defs_linear.append(nn.Linear(256, out_dim))
+
+    self.linops = nn.Sequential(*layer_defs_linear)
+
+  def _upsample_add(self, x, y):
+    '''Upsample and add two feature maps.
+    Args:
+      x: (Variable) top feature map to be upsampled.
+      y: (Variable) lateral feature map.
+    Returns:
+      (Variable) added feature map.
+    Note in PyTorch, when input size is odd, the upsampled feature map
+    with `F.upsample(..., scale_factor=2, mode='nearest')`
+    maybe not equal to the lateral feature map size.
+    e.g.
+    original input size: [N,_,15,15] ->
+    conv2d feature map size: [N,_,8,8] ->
+    upsampled feature map size: [N,_,16,16]
+    So we choose bilinear upsample which supports arbitrary output sizes.
+    '''
+    _, _, H, W = y.size()
+    return F.upsample(x, size=(H, W), mode='bilinear') + y
+
+  def forward(self, i):
+    x, sep = self._get_inputs(i)
+
+    # Bottom-up
+    c1 = self.layer0(x)
+
+    c1 += self.layer0_sep(sep)
+
+    c2 = self.layer1(c1)
+    c3 = self.layer2(c2)
+    c4 = self.layer3(c3)
+    c5 = self.layer4(c4)
+
+    # Top-down
+    p5 = self.toplayer(c5)
+    p4 = self._upsample_add(p5, self.latlayer1(c4))
+    p4 = self.smooth1(p4)
+    p3 = self._upsample_add(p4, self.latlayer2(c3))
+    p3 = self.smooth2(p3)
+    p2 = self._upsample_add(p3, self.latlayer3(c2))
+    p2 = self.smooth3(p2)
+
+    # Top-down predict and refine
+    d5, d4, d3, d2 = self.up1(self.agg1(p5)), self.up2(self.agg2(p4)), self.up3(self.agg3(p3)), self.agg4(p2)
+    _, _, H, W = d2.size()
+    vol = torch.cat([F.upsample(d, size=(H, W), mode='bilinear') for d in [d5,d4,d3,d2]], dim=1)
+    vol = self.predict5(self.predict4(self.predict3(self.predict2(self.predict1(vol))))).view(vol.shape[0],-1)
+    out = self.linops(vol)
+
+    if self.use_softmax:
+      out = F.softmax(out, 1)
+
+    return out
+
+  def _get_inputs(self, i):
+      input_frames = self.frame_buffer[i:i+FLAGS.context_frames]
+      # [time, channels, height, width] -> [time * channels, height, width]
+      input_frames = torch.reshape(input_frames, [-1, FLAGS.image_height, FLAGS.image_width])
+      current_frame = input_frames[-4:-1]
+      other = torch.cat([input_frames[:-4], input_frames[-1:], self.extra_channels[i]])
+      return current_frame.unsqueeze(0), other.unsqueeze(0)
